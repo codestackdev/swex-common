c07aa1fde0f5cc5083cb2b21f23d5e5cfd9cb985
CodeStack <codestack.net@gmail.com>
2019-10-04 19:24:16 +1000

    Implementd #7 - Add the attribute to load description from the resources
    Implemented #8 - Add method to extract SOLIDWORKS version and SP

---
+++ Base/Attributes/SummaryAttribute.cs
@@ -0,0 +1,36 @@
+﻿//**********************
+//SwEx - development tools for SOLIDWORKS
+//Copyright(C) 2019 www.codestack.net
+//License: https://github.com/codestackdev/swex-common/blob/master/LICENSE
+//Product URL: https://www.codestack.net/labs/solidworks/swex
+//**********************
+
+using CodeStack.SwEx.Common.Reflection;
+using System;
+using System.ComponentModel;
+
+namespace CodeStack.SwEx.Common.Attributes
+{
+    /// <summary>
+    /// Decorates the description for the element (e.g. command, user control, object etc.)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class SummaryAttribute : DescriptionAttribute
+    {
+        /// <summary>
+        /// Constructor for element summary
+        /// </summary>
+        /// <param name="description">Description of the element</param>
+        public SummaryAttribute(string description) : base(description)
+        {
+        }
+
+        /// <inheritdoc cref="SummaryAttribute(string)"/>
+        /// <param name="resType">Type of the static class (usually Resources)</param>
+        /// <param name="descriptionResName">Resource name of the string for display name</param>
+        public SummaryAttribute(Type resType, string descriptionResName)
+            : this(ResourceHelper.GetResource<string>(resType, descriptionResName))
+        {
+        }
+    }
+}
---
+++ Base/Extensions/SldWorksCommonEx.cs
@@ -0,0 +1,31 @@
+﻿using CodeStack.SwEx.Common.Enums;
+
+namespace SolidWorks.Interop.sldworks
+{
+    /// <summary>
+    /// Collection of common extension methods to use in the SwEx framework
+    /// </summary>
+    public static partial class SldWorksCommonEx
+    {
+        /// <summary>
+        /// Returns the major version of SOLIDWORKS application
+        /// </summary>
+        /// <param name="app">Pointer to application to return version from</param>
+        /// <returns>Major version of the application</returns>
+        public static SwVersion_e GetVersion(this ISldWorks app, out int minor)
+        {
+            var rev = app.RevisionNumber().Split('.');
+            var majorRev = int.Parse(rev[0]);
+            minor = int.Parse(rev[1]);
+
+            return (SwVersion_e)majorRev;
+        }
+
+        /// <inheritdoc cref="GetVersion(ISldWorks, out int)"/>
+        public static SwVersion_e GetVersion(this ISldWorks app)
+        {
+            int minor;
+            return app.GetVersion(out minor);
+        }
+    }
+}
